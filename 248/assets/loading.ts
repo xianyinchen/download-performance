09688dcd09e8680d846d5b9757b0d039a5a3de9f
kenshin <420373550@qq.com>
2022-03-09 19:53:21 +0800

    add 248

---
+++ 248/assets/loading.ts
@@ -0,0 +1,126 @@
+const { ccclass, property } = cc._decorator;
+
+@ccclass
+export default class Loading extends cc.Component {
+
+    @property(cc.Label)
+    animation: cc.Label = null;
+
+    @property(cc.Label)
+    atlas: cc.Label = null;
+
+    @property(cc.Label)
+    audio: cc.Label = null;
+
+    @property(cc.Label)
+    font: cc.Label = null;
+
+    @property(cc.Label)
+    particles: cc.Label = null;
+
+    @property(cc.Label)
+    prefab: cc.Label = null;
+
+    @property(cc.Label)
+    textures: cc.Label = null;
+
+    @property(cc.Label)
+    tmxs: cc.Label = null;
+
+    @property(cc.Label)
+    video: cc.Label = null;
+
+    @property(cc.Label)
+    models: cc.Label = null;
+
+    start() {
+        let time = (new Date()).getTime();
+        cc.assetManager.loadBundle('models', (err, res: cc.AssetManager.Bundle) => {
+            res.loadDir("list", (err, list) => {
+                this.models.string = `models = ${(new Date()).getTime() - time} - ${list.length}`;
+                this.loadImages();
+            })
+        })
+    }
+
+    loadImages() {
+        let time2 = (new Date()).getTime();
+        cc.assetManager.loadBundle('textures', (err, res: cc.AssetManager.Bundle) => {
+            res.loadDir("list", (err, list) => {
+                this.textures.string = `images = ${(new Date()).getTime() - time2} - ${list.length}`;
+                this.loadPrefabs();
+            })
+        })
+    }
+
+    loadPrefabs() {
+        let time2 = (new Date()).getTime();
+        cc.assetManager.loadBundle('prefab', (err, res: cc.AssetManager.Bundle) => {
+            res.loadDir("list", (err, list) => {
+                this.prefab.string = `prefabs = ${(new Date()).getTime() - time2} - ${list.length}`;
+                this.loadAudios();
+            })
+        })
+    }
+
+
+    loadAudios() {
+        let time2 = (new Date()).getTime();
+        cc.assetManager.loadBundle('audio', (err, res: cc.AssetManager.Bundle) => {
+            res.loadDir("list", (err, list) => {
+                this.audio.string = `audios = ${(new Date()).getTime() - time2} - ${list.length}`;
+                this.loadPlists();
+            })
+        })
+    }
+
+    loadPlists() {
+        let time2 = (new Date()).getTime();
+        cc.assetManager.loadBundle('atlas', (err, res: cc.AssetManager.Bundle) => {
+            res.loadDir("list", (err, list) => {
+                this.atlas.string = `plists = ${(new Date()).getTime() - time2} - ${list.length}`;
+                this.loadTmxs();
+            })
+        })
+    }
+
+    loadTmxs() {
+        let time2 = (new Date()).getTime();
+        cc.assetManager.loadBundle('tmxs', (err, res: cc.AssetManager.Bundle) => {
+            res.loadDir("list", (err, list) => {
+                this.tmxs.string = `tmxs = ${(new Date()).getTime() - time2} - ${list.length}`;
+                this.loadVideos();
+            })
+        })
+    }
+
+
+    loadVideos() {
+        let time2 = (new Date()).getTime();
+        cc.assetManager.loadBundle('video', (err, res: cc.AssetManager.Bundle) => {
+            res.loadDir("list", (err, list) => {
+                this.video.string = `videos = ${(new Date()).getTime() - time2} - ${list.length}`;
+                this.loadAnims();
+            })
+        })
+    }
+
+    loadAnims() {
+        let time2 = (new Date()).getTime();
+        cc.assetManager.loadBundle('animation', (err, res: cc.AssetManager.Bundle) => {
+            res.loadDir("list", (err, list) => {
+                this.animation.string = `anims = ${(new Date()).getTime() - time2} - ${list.length}`;
+                this.loadFont();
+            })
+        })
+    }
+
+    loadFont() {
+        let time2 = (new Date()).getTime();
+        cc.assetManager.loadBundle('font', (err, res: cc.AssetManager.Bundle) => {
+            res.loadDir("list", (err, list) => {
+                this.animation.string = `font = ${(new Date()).getTime() - time2} - ${list.length}`;
+            })
+        })
+    }
+}
---
+++ 248/packages/hot-update/main.js
@@ -0,0 +1,156 @@
+'use strict';
+
+const os = require('os');
+const Fs = require("fs");
+const Path = require("path");
+
+function getIPAdress() {
+    var interfaces = os.networkInterfaces();
+    for (var devName in interfaces) {
+        var iface = interfaces[devName];
+        for (var i = 0; i < iface.length; i++) {
+            var alias = iface[i];
+            if (alias.family === 'IPv4' && alias.address !== '127.0.0.1' && !alias.internal) {
+                return alias.address;
+            }
+        }
+    }
+}
+
+// 引入http模块
+const http = require("http");
+
+var remote_server = null;
+function createServer(ip, port) {
+    try {
+        if (null != remote_server) {
+            remote_server.close();
+        }
+
+        // 创建server，指定处理客户端请求的函数
+        Editor.log("IP: " + ip + ":" + port);
+        remote_server = http.createServer(
+            function (request, response) {
+                //判断HTTP方法，只处理GET 
+                if (request.method != "GET") {
+                    response.writeHead(403);
+                    response.end();
+                    return null;
+                }
+
+                //此处也可使用URL模块来分析URL(https://nodejs.org/api/url.html)
+                var sep = request.url.indexOf('?');
+                var filePath = sep < 0 ? request.url : request.url.slice(0, sep);
+
+                //当文件存在时发送数据给客户端，否则404
+                var fileStat = Fs.stat(Path.join(__dirname, "../../", "." + filePath),
+                    function (err, stats) {
+                        Editor.log("GET file: " + Path.join(__dirname, "../../", "." + filePath));
+
+                        if (err) {
+                            response.writeHead(404);
+                            response.end();
+                            return null;
+                        }
+                        //TODO:Content-Type应该根据文件类型设置
+                        response.writeHead(200, { "Content-Type": "text/plain", "Content-Length": stats.size });
+
+                        //使用Stream
+                        var stream = Fs.createReadStream(Path.join(__dirname, "../../", "." + filePath));
+
+                        stream.on('data', function (chunk) {
+                            response.write(chunk);
+                        });
+
+                        stream.on('end', function () {
+                            response.end();
+                        });
+
+                        stream.on('error', function () {
+                            response.end();
+                        });
+                    }
+                );
+            }
+        ).listen(port);
+    }
+    catch (e) {
+        Editor.log(e);
+    }
+}
+
+const copyDir = function (src, dst) {
+    if (!Fs.existsSync(src)) return;
+    var paths = Fs.readdirSync(src);
+
+    if (!Fs.existsSync(dst)) Fs.mkdirSync(dst);
+    paths.forEach(function (path) {
+        const _src = src + '/' + path
+        const _dst = dst + '/' + path
+        let readable; let writable
+        var st = Fs.statSync(_src);
+        if (st.isFile()) {
+            readable = Fs.createReadStream(_src)
+            writable = Fs.createWriteStream(_dst)
+            readable.pipe(writable)
+        }
+        else if (st.isDirectory()) {
+            if (!Fs.existsSync(dst))
+                Fs.mkdirSync(dst);
+            copyDir(_src, _dst);
+        }
+    })
+}
+
+const rmdirSync = function (path) {
+    if (Fs.existsSync(path)) {
+        Fs.readdirSync(path).forEach(function (file) {
+            var curPath = path + "\\" + file;
+            if (Fs.statSync(curPath).isDirectory()) {
+                rmdirSync(curPath);
+            } else {
+                Fs.unlinkSync(curPath);
+            }
+        });
+        //Fs.rmdirSync(path + '\\');
+    }
+};
+
+module.exports = {
+    load: function () {
+        // 当 package 被正确加载的时候执行
+    },
+
+    unload: function () {
+        // 当 package 被正确卸载的时候执行
+    },
+
+    messages: {
+        "editor:build-finished": function (event, target) {
+            try {
+                var root = Path.normalize(target.dest);
+
+                copyDir(Path.join(root, '/remote'), Path.join(__dirname, '../../remote'));
+                rmdirSync(Path.join(root, '/remote'));
+
+                // 配置远程资源地址
+
+                var url = Path.join(root, "/src/settings.js");
+
+                var srcStr = Fs.readFileSync(url, 'utf-8');
+                Fs.writeFileSync(url, srcStr.replace("nochange", `http://${getIPAdress()}:${5500}`));
+
+                // 启动不检测 http 合法性
+
+                var url = Path.join(root, "/project.config.json");
+
+                var srcStr = Fs.readFileSync(url, 'utf-8');
+                Fs.writeFileSync(url, srcStr.replace(`"urlCheck": true`, `"urlCheck": false`));
+
+                createServer(getIPAdress(), 5500);
+            } catch (error) {
+                Editor.log(error)
+            }
+        }
+    }
+};
